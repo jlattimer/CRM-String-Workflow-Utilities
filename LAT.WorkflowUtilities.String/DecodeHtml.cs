d3a2b18419caf4525f7cdd88e44c765846bd420e
Kelvin Shen <kelvin.shen@fusion5.co.nz>
2017-04-17 19:40:40 +1200

    Added decode and encode html text WF activities.

---
+++ LAT.WorkflowUtilities.String/DecodeHtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Net;
+
+namespace LAT.WorkflowUtilities.String
+{
+    public class DecodeHtml : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("String To Decode")]
+        public InArgument<string> StringToDecode { get; set; }
+
+        [Output("Decoded String")]
+        public OutArgument<string> DecodedString { get; set; }
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            ITracingService tracer = executionContext.GetExtension<ITracingService>();
+
+            try
+            {
+                string strToDecode = StringToDecode.Get(executionContext);
+                string decodedString = WebUtility.HtmlDecode(strToDecode);
+                DecodedString.Set(executionContext, decodedString);
+            }
+            catch (Exception e)
+            {
+                tracer.Trace("Exception: {0}", e.ToString());
+                throw new InvalidPluginExecutionException(e.Message);
+            }
+        }
+    }
+}
---
+++ LAT.WorkflowUtilities.String/EncodeHtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Net;
+
+namespace LAT.WorkflowUtilities.String
+{
+    public class EncodeHtml : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("String To Encode")]
+        public InArgument<string> StringToEncode { get; set; }
+
+        [Output("Encoded String")]
+        public OutArgument<string> EncodedString { get; set; }
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            ITracingService tracer = executionContext.GetExtension<ITracingService>();
+
+            try
+            {
+                string strToEncode = StringToEncode.Get(executionContext);
+                string encodedString = WebUtility.HtmlEncode(strToEncode);
+                EncodedString.Set(executionContext, encodedString);
+            }
+            catch (Exception e)
+            {
+                tracer.Trace("Exception: {0}", e.ToString());
+                throw new InvalidPluginExecutionException(e.Message);
+            }
+        }
+    }
+}
